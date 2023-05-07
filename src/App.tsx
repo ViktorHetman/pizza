647c32b7caa7881cf42a97965790a0a246359001
Viktor Hetman <vitya.getman.79@gmail.com>
2023-05-07 20:23:22 +0300

    splitting on chunks 2 pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
 import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
 
 import './scss/app.scss';
+
+const Cart = React.lazy(() => import('./pages/Cart'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
+        <Route
+          path="cart"
+          element={
+            <React.Suspense fallback={<div>Загрузка корзины...</div>}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
         <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="*"
+          element={
+            <React.Suspense fallback={<div>Загрузка страницы...</div>}>
+              <NotFound />
+            </React.Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
